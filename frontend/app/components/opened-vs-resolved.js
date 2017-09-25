2d6ebeeb080c2002ff82e0056f3dd9ef882e770b
Ryan Hollister <ryan.hollister@q2ebanking.com>
2017-09-25 09:16:52 -0500

    updated retail KPIs

--- frontend/app/components/opened-vs-resolved.js
+++ frontend/app/components/opened-vs-resolved.js
@@ -2,44 +2,9 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
-    activeKey: null,
+    activeKeys: [],
     switch: false,
     model: [],
-    axis: {
-        x: {
-            type: 'timeseries',
-            tick: {
-                format: '%Y-%m-%d'
-            }
-        }
-    },
-    data: Ember.computed('model', 'activeKey', function() {
-        var data = {
-            x: 'x',
-            columns: []
-        };
-        if (!this.get('activeKey')) {
-            return;
-        }
-        data.columns.push(this.get('dates'));
-        //data.columns.push(this.get('created'));
-        data.columns.push(this.get('resolved'));
-        this.toggleProperty('switch');
-        return data;
-    }),
-    dates: Ember.computed('model.[]', 'activeKey', function() {
-        return ['x'].concat(this.get('model').map((datapoint) => {
-            return moment(datapoint.date).format('YYYY-MM-DD');
-        }));
-    }),
-    created: Ember.computed('model.[]', 'activeKey', function() {
-        return []; //['opened'].concat(this.get('model').map((datapoint) => {
-        //     return 3000;
-        // }));
-    }),
-    resolved: Ember.computed('model.[]', 'activeKey', function() {
-        return ['resolved'].concat(this.get('model').map((datapoint) => {
-            return Ember.get(datapoint, this.get('activeKey'));
-        }));
-    })
+    axis: null,
+    data: null,
 });
--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -2,21 +2,79 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     activeKey: Ember.computed('_projectKey', '_subTypeKey', '_metricsKey', function() {
-        if (!this.get('_projectKey') || !this.get('_subTypeKey') || !this.get('_metricsKey')) {
+        if (!this.get('_projectKey') || !this.get('_metricsKey')) {
             return null;
         }
         return this.get('_projectKey') + this.get('_subTypeKey') + this.get('_metricsKey');
     }),
-    _projectKey: null,
-    _subTypeKey: null,
-    _metricsKey: null,
+    percentToggle: false,
+    _projectKey: '',
+    _subTypeKey: '',
+    _metricsKey: '',
+    storedMetrics: [],
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d'
+            }
+        },
+        y: {
+            show: false,
+        },
+        y2: {
+            show: false,
+            tick: {
+                format: d3.format('%')
+            }
+        }
+    },
+    dates: Ember.computed('model.[]', 'activeKeys.[]', function() {
+        return ['x'].concat(this.get('model').map((datapoint) => {
+            return moment(datapoint.date).format('YYYY-MM-DD');
+        }));
+    }),
+    data: Ember.computed('storedMetrics.[]', 'storedMetrics', function() {
+        var data = {
+            x: 'x',
+            columns: [],
+            axes: {}
+        };
+        if (Ember.isEmpty(this.get('storedMetrics'))) {
+            return data;
+        }
+        data.columns.push(this.get('dates'));
+        this.get('axis.y2.show', false);
+        this.get('axis.y.show', false);
+        this.get('storedMetrics').forEach((key) => {
+            let parentKey;
+            if (key.indexOf('%') > -1) {
+                parentKey = key.replace(/([a-zA-Z]*)\.total/, 'total');
+                parentKey = parentKey.replace('%', '');
+            }
+            let object =  [key].concat(this.get('model').map((datapoint) => {
+                let data = Ember.get(datapoint, key = key.replace('%', ''));
+                if (parentKey) {
+                    return data / Ember.get(datapoint, parentKey);
+                }
+                return data
+            }));
+            data.columns.push(object);
+            if (parentKey) {
+               data.axes[key] = 'y2'; 
+               this.set('axis.y2.show', true);
+            } else {
+                this.set('axis.y.show', true);
+            }
+        });
+        
+        this.toggleProperty('switch');
+        return data;
+    }),
     session: Ember.inject.service('session'),
     projects: Ember.computed('model.[]', function() {
         return [
-            { 'label': 'UUX', value: 'Q2.UUX' },
-            { 'label': 'UUX - Retail', value: 'Q2.UUX.Retail' },
-            { 'label': 'UUX - Commercial', value: 'Q2.UUX.Commercial' },
-            { 'label': 'HQ', value: 'Q2.HQ' }
+            { 'label': 'UUX', value: 'Q2.UUX' }
         ];
     }),
     subType: Ember.computed('model.[]', function() {
@@ -41,6 +99,19 @@ export default Ember.Controller.extend({
         ];
     }),
     actions: {
+        togglePercent() {
+            this.toggleProperty('percentToggle');
+        },
+        clear() {
+            this.set('storedMetrics', []);
+        },
+        add_metric() {
+            var key = this.get('activeKey');
+            if (this.get('percentToggle')) {
+                key = '%' + this.get('activeKey');
+            }
+            this.get('storedMetrics').pushObject(key);
+        },
         projectChange(value) {
             this.set('_projectKey', value);
         },
@@ -48,7 +119,11 @@ export default Ember.Controller.extend({
             this.set('_metricsKey', '.' + value);
         },
         subTypeChange(value) {
-            this.set('_subTypeKey', '.' + value);
+            if (value) {
+                this.set('_subTypeKey', '.' + value);
+                return;
+            }
+            this.set('_subTypeKey', '');
         }
     }
 });
